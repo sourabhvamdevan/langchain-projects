89442db96881d291c567546e0b62e05076940fdd
Sourabh Vamdevan <44829214+sourabhvamdevan@users.noreply.github.com>
2026-03-03 14:39:48 +0530

    Insert historical context for the word 'loot'
    Added a passage about the word 'loot' and its historical context.

--- langchain-output/typeDict-with-stuctured-output.py
+++ langchain-output/typeDict-with-stuctured-output.py
@@ -25,6 +25,12 @@ structured_model=model.with_structured_output(Review)
 
 
 result = structured_model.invoke("""
+One of the very first Indian words to enter the English language was the Hindustani slang for plunder: “loot”. According to the Oxford English Dictionary, 
+    this word was rarely heard outside the plains of north India until the late 18th century, when it suddenly became a common term across Britain. 
+    To understand how and why it took root and flourished in so distant a landscape, one need only visit Powis Castle.
+    The last hereditary Welsh prince, Owain Gruffydd ap Gwenwynwyn, built Powis castle as a craggy fort in the 13th century; 
+    the estate was his reward for abandoning Wales to the rule of the English monarchy. But its most spectacular treasures date from a much later period of English conquest and appropriation: 
+    Powis is simply awash with loot from India, room after room of imperial plunder, extracted by the East India Company in the 18th century.
                     """)
 print(result)
 
